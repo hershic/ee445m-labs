84e86f3d7051e934f15c4a5dfd00801c022096a2
Hershal Bhave <hershal.bhave@gmail.com>
2015-01-25 22:32:11 -0600

    checkpoint... done

--- lib/libuart/uart.h
+++ lib/libuart/uart.h
@@ -1,3 +1,17 @@
-void configure_uart();
+#include <stdbool.h>
+
+void uart_set_active_channel(const long channel);
+void uart_clear_active_channel();
+bool uart_has_active_channel();
+
+void uart_init();
+void uart_init_(const long channel);
+
 void uart_send(const char* text);
-void uart_recv();
+void uart_send_(const long channel, const char* text);
+
+char uart_get_char();
+char uart_get_char_(const long channel);
+
+char* uart_get_string(const long string_length);
+char* uart_get_string_(const long channel, const long string_length);
