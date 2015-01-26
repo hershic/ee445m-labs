28e5483d8b5c24ee6b2461b667901fda944ad00e
Hershal Bhave <hershal.bhave@gmail.com>
2015-01-25 18:39:46 -0600

    added test uart project

---
+++ board-progs/test-uart/test-uart.c
@@ -0,0 +1,57 @@
+/* Created by Hershal Bhave and Eric Crosson on 2015-01-24 */
+/* Revision History: Look in Git FGT */
+
+/* Standard Libs */
+#include <stdint.h>
+#include <stdbool.h>
+
+/* TI Includes */
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+
+/* Driverlib Includes */
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/rom.h"
+
+#include "libuart/uart.h"
+
+
+void UART0_Handler(void) {
+    uart_recv();
+}
+
+int main(void) {
+
+    FPUEnable();
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_16MHZ);
+
+    /* Enable the GPIO port that is used for the on-board LED. */
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    /* Enable the GPIO pins for the LED (PF2). */
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+
+    /* Enable the peripherals used by this example. */
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    /* Enable processor interrupts. */
+    IntMasterEnable();
+
+    /* Set GPIO A0 and A1 as UART pins. */
+    uart_init();
+
+    /* Prompt for text to be entered. */
+    /* UARTSend((uint8_t *)"\033[2JEnter text: ", 16); */
+
+    uart_send("Enter text:");
+
+    /* Do nothing */
+    while (1) {}
+}
