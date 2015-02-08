7bf6470ad58479ddcc911d64cbf26948eec8697d
Eric Crosson <esc@ericcrosson.com>
2015-02-08 13:46:13 -0600

    transferred test-uart to libheart api
    for testing, all checks out

--- board-progs/test-uart/test-uart.c
+++ board-progs/test-uart/test-uart.c
@@ -22,6 +22,7 @@
 #include "driverlib/rom.h"
 
 #include "libuart/uart.h"
+#include "libheart/heartbeat.h"
 
 #include <sys/stat.h>
 
@@ -41,13 +42,13 @@ void UART0_Handler(void) {
         free(received_string);
 
         /* Blink the LED to show a character transfer is occuring. */
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+	heart_beat();
 
         /* Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks. */
         SysCtlDelay(SysCtlClockGet() / (1000 * 3));
 
         /* Turn off the LED */
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+        heart_off();
     }
 }
 
