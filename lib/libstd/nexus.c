d0a5a1f5e4bbe9c8a69ebcd52acc125e38bf7519
Eric Crosson <esc@ericcrosson.com>
2015-02-13 00:40:26 -0600

    removed outclassed function

--- lib/libstd/nexus.c
+++ lib/libstd/nexus.c
@@ -1,14 +1,6 @@
 /* -*- mode: c; c-basic-offset: 4; -*- */
 #include "nexus.h"
 
-/* char* itoa(int i, char* buffer, uchar length) { */
-
-/*     if (snprintf(buffer, length, "%02d", i) == -1) { */
-/* 	return ""; /\* base case, or error *\/ */
-/*     } */
-/*     return buffer; */
-/* } */
-
 void* memset(void* b, int c, int len) {
 
     int i;
--- lib/libstd/nexus.h
+++ lib/libstd/nexus.h
@@ -5,15 +5,7 @@
 #include "defines.h"
 #include <stdint.h>
 
-/*! Convert an integer to a string.
- *  \brief Convert an integer to a string.
- *  \param i Int to convert into a string
- *  \param buffer Buffer for string contents
- *  \param length Length of allocated buffer
- *  \returns char* Buffer containing i represented as a string
- *  \ingroup Framebuffer
- */
-/* char* itoa(int, char*, unsigned char); */
+/* TODO: doxytize */
 
 inline static
 void postpone_death() {
