c8ea435b5da402fbab5e10a93b9f09635651932f
Eric Crosson <esc@ericcrosson.com>
2015-02-16 14:17:13 -0600

    libheart has grown to include arbitrary GPIO manip
    From the .dox file:
    
    This library provides both direct access to the colloquial 'heart'
    (on-board LED commonly used for diagnosis of the patient) and the
    ability to manipulate arbitrary GPIO ports as one would the
    heart. Functions ending without an underscore manipulate the
    \HEART_MUSCLE, while functions ending with an underscore belong to a set
    of general GPIO manipulation functions. This is to say each function in
    the following code block will act on the same GPIO pin, GPIO_PIN_3 on
    GPIO_PORTE_BASE, EXCEPT the last function call which will toggle
    \HEART_MUSCLE.
    
    \code
    muscle_t portEpin3;
    portEpin3.base = GPIO_PORTE_BASE;
    portEpin3.pin = GPIO_PIN_3;
    
    heart_pump_(&portEpin3);                               // initialize pin and port base
    int32_t porte_pin3_status = heart_status_(&portEpin3); // read the status of pin 3 of port E
    heart_beat_(&portEpin3);                               // toggle pin 3 of port E twice
    heart_toggle();                                        // refers to \HEART_MUSCLE
    \endcode
    
    etc.

--- lib/libheart/heartbeat.h
+++ lib/libheart/heartbeat.h
@@ -2,8 +2,35 @@
 #ifndef __HEARTBEAT__
 #define __HEARTBEAT__
 
+/* Is there a way to clean this up? I feel like I'm re-inventing the wheel. */
+/*! A pointer to a memory location on the ARM Cortex M4. */
+typedef int32_t memory_address_t;
+
+/*! Contains information to identify a GPIO pin. This includes the
+ *  port base and the specific pin. */
+typedef struct muscle_t {
+    memory_address_t base;
+    memory_address_t pin;
+} muscle_t;
+
+/*! A skeletal muscle that likes to pretend it's smooth. This */
+muscle_t HEART_ANCILLARY_MUSCLE;
+
+/*! The on-board LED colloquially referred to as the 'heart.' Does
+ *  every computer have one? */
+#define HEART_MUSCLE GPIO_PIN_2
+
+/*! The GPIO base where the \HEART_MUSCLE may be found. Otherwise
+ *  knows as a subdivision of the ventral body cavity which houses the
+ *  heart. */
+#define THORACIC_CAVITY GPIO_PORTF_BASE
+
+/*
+ * Begin non-modal API -- implicit subject is \HEART_MUSCLE.
+ */
+
 /*!
- *  \brief Initialize the heart for visible transformation
+ *  \brief Initialize \HEART_MUSCLE for visible transformation.
  *  \returns void
  *  \ingroup Heart
  */
@@ -14,33 +41,35 @@ void heart_init() {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
     /* Enable the GPIO pins for the LED (PF2). */
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+    GPIOPinTypeGPIOOutput(THORACIC_CAVITY, HEART_MUSCLE);
 }
 
 /*!
- *  \brief Turn the heart off
+ * \brief Return the status of \HEART_MUSCLE.
+ * \returns The current status of \HEART_MUSCLE.
+ * \ingroup Heart
+ */
+inline
+int32_t heart_status() {GPIOPinRead(THORACIC_CAVITY, HEART_MUSCLE);}
+
+/*!
+ *  \brief Turn \HEART_MUSCLE off.
  *  \returns void
  *  \ingroup Heart
  */
 inline
-void heart_off() {
-
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-}
+void heart_off() {GPIOPinWrite(THORACIC_CAVITY, HEART_MUSCLE, 0);}
 
 /*!
- *  \brief Turn the heart on
+ *  \brief Turn \HEART_MUSCLE on.
  *  \returns void
  *  \ingroup Heart
  */
 inline
-void heart_on() {
-
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 1);
-}
+void heart_on() {GPIOPinWrite(THORACIC_CAVITY, HEART_MUSCLE, 1);}
 
 /*!
- *  \brief Toggle the heart once
+ *  \brief Toggle \HEART_MUSCLE once.
  *  \details end_state = ~starting_state
  *  \returns void
  *  \ingroup Heart
@@ -48,12 +77,11 @@ void heart_on() {
 inline
 void heart_toggle() {
 
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2,
-		 GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_2) ^ GPIO_PIN_2);
+    GPIOPinWrite(THORACIC_CAVITY, HEART_MUSCLE, heart_status() ^ HEART_MUSCLE);
 }
 
 /*!
- *  \brief Toggle the heart twice
+ *  \brief Toggle \HEART_MUSCLE twice.
  *  \details end_state = starting_state
  *  \returns void
  *  \ingroup Heart
@@ -61,11 +89,106 @@ void heart_toggle() {
 inline
 void heart_beat() {
 
-    heart_toggle();
-    heart_toggle();
+    GPIOPinWrite(THORACIC_CAVITY, HEART_MUSCLE, heart_status() ^ HEART_MUSCLE);
+    GPIOPinWrite(THORACIC_CAVITY, HEART_MUSCLE, heart_status() ^ HEART_MUSCLE);
 }
 
-/* TOOD: use bind (or equivalent function) to wrap a fn pointer with
+/*
+ * Begin modal API -- concentrate on a muscle and pump it baby
+ */
+
+/*! \brief An alias for \heart_init_. I like the imagery. */
+#define heart_pump_(a, b) heart_init_(a, b)
+
+/*!
+ *  \brief Initialize \HEART_ANCILLARY_MUSCLE for visible
+ *  transformation and set the modal state. That is to say, all modal
+ *  function calls will act on \gpio_base and \gpio_pin until another
+ *  mode is intialized.
+ *  \param The muscle to use as an ancillary heart.
+ *  \returns void
+ *  \ingroup Heart
+ *  \bug Ensure your SYSCTL_PERIPH_GPIOx is enabled, this fn isn't
+ *  smart enough to do it yet.
+ */
+inline
+void heart_init_(muscle_t* ancillary_muscle) {
+
+    /* Enable the GPIO port that is used for \HEART_ANCILLARY_MUSCLE. */
+    /* TODO: ensure your GPIO peripheral is enabled */
+    /* SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); */
+
+    /* Enable the GPIO pins for \HEART_ANCILLARY_MUSCLE. */
+    GPIOPinTypeGPIOOutput(ancillary_muscle->base, ancillary_muscle->pin);
+}
+
+/*!
+ * \brief Return the status of \HEART_ANCILLARY_MUSCLE.
+ *  \param The ancillary muscle to act upon.
+ * \returns The current status of \HEART_ANCILLARY_MUSCLE.
+ * \ingroup Heart
+ */
+inline
+int32_t heart_status_(muscle_t* ancillary_muscle) {
+
+    GPIOPinRead(ancillary_muscle->base, ancillary_muscle->pin);
+}
+
+/*!
+ *  \brief Turn \ancillary_muscle off.
+ *  \param The ancillary muscle to act upon.
+ *  \returns void
+ *  \ingroup Heart
+ */
+inline
+void heart_off_(muscle_t* ancillary_muscle) {
+
+    GPIOPinWrite(ancillary_muscle->base, ancillary_muscle->pin, 0);
+}
+
+/*!
+ *  \brief Turn \ancillary_muscle on.
+ *  \param The ancillary muscle to act upon.
+ *  \returns void
+ *  \ingroup Heart
+ */
+inline
+void heart_on_(muscle_t* ancillary_muscle) {
+
+    GPIOPinWrite(ancillary_muscle->base, ancillary_muscle->pin, 1);
+}
+
+/*!
+ *  \brief Toggle \ancillary_muscle once.
+ *  \details end_state = ~starting_state
+ *  \param The ancillary muscle to act upon.
+ *  \returns void
+ *  \ingroup Heart
+ */
+inline
+void heart_toggle_(muscle_t* ancillary_muscle) {
+
+    GPIOPinWrite(ancillary_muscle->base, ancillary_muscle->pin,
+		 heart_status_(ancillary_muscle) ^ ancillary_muscle->pin);
+}
+
+/*!
+ *  \brief Toggle \ancillary_muscle twice.
+ *  \details end_state = starting_state
+ *  \param The ancillary muscle to act upon.
+ *  \returns void
+ *  \ingroup Heart
+ */
+inline
+void heart_beat_(muscle_t* ancillary_muscle) {
+
+    GPIOPinWrite(ancillary_muscle->base, ancillary_muscle->pin,
+		 heart_status_(ancillary_muscle) ^ ancillary_muscle->pin);
+    GPIOPinWrite(ancillary_muscle->base, ancillary_muscle->pin,
+		 heart_status_(ancillary_muscle) ^ ancillary_muscle->pin);
+}
+
+/* FEATURE TODO: use bind (or equivalent function) to wrap a fn pointer with
  * beat(), ptr(), toggle() and return prt()'s ret value */
 
 #endif
