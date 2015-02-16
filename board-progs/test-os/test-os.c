e4889987c5c842f8d8f556ca5d948ebf89c805fd
Hershal Bhave <hershal.bhave@gmail.com>
2015-02-16 11:26:43 -0600

    preempt checkout session

--- board-progs/test-os/test-os.c
+++ board-progs/test-os/test-os.c
@@ -37,6 +37,31 @@ void turn_off_led() {
     }
 }
 
+/* VALVANO CRAP */
+uint32_t Count1, Count2, Count3;
+void Thread1b(void){
+    Count1 = 0;
+    for(;;){
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_PIN_4 ^ GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4));
+        Count1++;
+    }
+}
+void Thread2b(void){
+    Count2 = 0;
+    for(;;){
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3 ^ GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_3));
+        Count2++;
+    }
+}
+void Thread3b(void){
+    Count3 = 0;
+    for(;;){
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2 ^ GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_2));
+        Count3++;
+    }
+}
+/* END VALVANO CRAP */
+
 int main() {
 
     SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
@@ -47,12 +72,17 @@ int main() {
 
     /* Enable the GPIO pins for the LED (PF2). */
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_4);
 
     IntMasterDisable();
 
     os_threading_init();
-    os_add_thread(turn_on_led);
-    os_add_thread(turn_off_led);
+    /* os_add_thread(turn_on_led); */
+    /* os_add_thread(turn_off_led); */
+    os_add_thread(Thread1b);
+    os_add_thread(Thread2b);
+    os_add_thread(Thread3b);
 
     /* Load and enable the systick timer */
     SysTickPeriodSet(SysCtlClockGet() / 10);
@@ -60,9 +90,6 @@ int main() {
     SysTickIntEnable();
 
     os_launch();
-
-    /* Don't get here yet, still testing if everything else works! */
-    /* os_launch(0); */
 
     /* And we're done; this should never execute */
     while (1) {}
--- toolchain-gcc-arm-embedded.cmake
+++ toolchain-gcc-arm-embedded.cmake
@@ -50,7 +50,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 set(CMAKE_C_FLAGS
   "${CMAKE_C_FLAGS} -Werror"
-  "-g -fno-common -ffunction-sections -fdata-sections -fomit-frame-pointer -Dgcc -DPART_TM4C123GH6PM"
+  "-O3 -fno-common -ffunction-sections -fdata-sections -fomit-frame-pointer -Dgcc -DPART_TM4C123GH6PM"
 )
 
 if (CMAKE_SYSTEM_PROCESSOR STREQUAL "cortex-m4")
