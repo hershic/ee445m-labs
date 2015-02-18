2cbaccd3f03a76c3b3090262f95998e60026dfa6
Eric Crosson <esc@ericcrosson.com>
2015-02-18 12:50:50 -0600

    generalized doxygen docs

--- lib/libos/semaphore.h
+++ lib/libos/semaphore.h
@@ -5,30 +5,30 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-/*! Implements a simple spinlock-based locking mechanism. 
- *  \param blocker block until this value is nonzero.
+/*! Implements a simple spinlock-based locking mechanism.
+ *  \param Block until this value is nonzero.
  */
-void spinlock_until(int32_t* blocker);
+void spinlock_until(int32_t*);
 
 /*! Semaphore typedef */
 typedef int32_t sem_t;
 
 /*! Initializes a blocking semaphore
- *  \param sem the semaphore to initialize.
- *  \param initial_value the maximum number of concurrent consumers of
+ *  \param The semaphore to initialize.
+ *  \param The maximum number of concurrent consumers of
  *  the resource guarded by the semaphore.
  */
-void sem_init(sem_t* sem, int32_t initial_value);
+void sem_init(sem_t*, int32_t);
 
 /*! Post (release) a semaphore lock. This increments the value of the
  *  semaphore so that additional resources may be consumed.
- *  \param sem the semaphore to post (release).
+ *  \param The semaphore to post (release).
  */
-void sem_post(sem_t* sem);
+void sem_post(sem_t*);
 
 /*! Wait on (block) a semaphore. This waits until the value of the
  *  semaphore is larger than zero to unblock the current thread. The
  *  value of the semaphore is then decremented to consume the resource.
- *  \param sem the semaphore to wait on (block).
+ *  \param The semaphore to wait on (block).
  */
-void sem_wait(sem_t* sem);
+void sem_wait(sem_t*);
