58d5b9c82dc429bc21eea2094bafddce4c5087b3
Hershal Bhave <hershal.bhave@gmail.com>
2015-02-18 01:18:43 -0600

    another minor grammer fix
    Let's see if this post-receive hook works now
    
    email=true

--- lib/libos/semaphore.h
+++ lib/libos/semaphore.h
@@ -6,7 +6,7 @@
 #include <stdint.h>
 
 /*! Implements a simple spinlock-based locking mechanism. 
- *  \param blocker the spinlock will wait until this value is nonzero.
+ *  \param blocker block until this value is nonzero.
  */
 void spinlock_until(int32_t* blocker);
 
