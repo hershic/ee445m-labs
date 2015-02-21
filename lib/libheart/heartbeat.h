08b2ae931018ee29bcf87944613a06db2bee5676
Eric Crosson <esc@ericcrosson.com>
2015-02-21 02:25:47 -0600

    moved todo logically

--- lib/libheart/heartbeat.h
+++ lib/libheart/heartbeat.h
@@ -312,9 +312,9 @@ muscle_t* heart_hew_muscle_(muscle_t*        muscle,
     /* A convenience for the client developer */
     return muscle;
 }
+#endif /* HEARTBEAT_MODAL */
 
 /* FEATURE TODO: use bind (or equivalent function) to wrap a fn pointer with
  * beat(), ptr(), toggle() and return prt()'s ret value */
-#endif /* HEARTBEAT_MODAL */
 
 #endif
