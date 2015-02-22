ac89d4c4157dfaacfcc3616a35b53c01da909d2c
Eric Crosson <esc@ericcrosson.com>
2015-02-21 18:49:41 -0600

    getting lost here
    will you try to always include client code somewhere?
    mind finishing this up?
    
    email=true

---
+++ board-progs/test-semaphore/test-semaphore.c
@@ -0,0 +1,56 @@
+/* -*- mode: c; c-basic-offset: 4; -*- */
+/* Created by Eric Crosson on 2015-02-20 */
+/* Revision History: Look in Git FGT */
+
+/* Standard Libs */
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* TI Includes */
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+
+/* Driverlib Includes */
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/rom.h"
+
+#define HEARTBEAT_MODAL
+#include "libos/os.h"
+#include "libheart/heartbeat.h"
+
+void main(void) {
+
+  SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+		 SYSCTL_XTAL_16MHZ);
+
+  IntMasterDisable();
+
+  os_threading_init();
+  os_add_thread(Thread1);
+  os_add_thread(Thread2);
+  os_add_thread(Thread3);
+
+  /* Load and enable the systick timer */
+  SysTickPeriodSet(SysCtlClockGet() / 10);
+  SysTickEnable();
+  SysTickIntEnable();
+
+  /* os_trap_ */
+  os_launch();
+
+  /* PONDER: why do interrupts fire without this? */
+  IntMasterEnable();
+
+  int32_t sem;
+  sem_init(&sem, 1);
+  while(1) {
+    sem_wait
+  }
+}
