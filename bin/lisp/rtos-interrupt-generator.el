4bac5d9dde08b462448c24b7bb5b1b852de8fda3
Eric Crosson <esc@ericcrosson.com>
2015-02-21 18:20:20 -0600

    separation of code and data

--- bin/lisp/rtos-interrupt-generator.el
+++ bin/lisp/rtos-interrupt-generator.el
@@ -8,6 +8,31 @@
 
 ;;; Code:
 
+(defvar rtos/interrupt-template-alist nil
+  "Contains (DEVICE . TEMPLATE) of interrupt handlers.")
+(setq rtos/interrupt-template-alist
+      '((uart . "unsigned short i;
+    hw_notification notification;
+    /* TODO: determine which bit to clear:  */
+    unsigned long look_at_me = UARTIntStatus();
+    /* UARTIntClear(%s_BASE, ); */
+
+    while(UARTCharsAvail(%s_BASE)) {
+
+	/* Notify every subscribed task of each incoming character
+	 * (but schedule them for later so we can return from this ISR
+	 * asap). */
+	notification._char = uart_get_char();
+
+	/* TODO: schedule this thread instead of running it immediately */
+	hw_notify(HW_UART, %s_BASE, notification);
+    }")
+	(timer . "TimerIntClear(%s_BASE, TIMER_TIMA_TIMEOUT);
+    hw_notification notification;
+    notification._int = 1;
+    hw_notify(HW_TIMER, %s_BASE, notification);")
+	))
+
 (defun combinations (&rest lists)
   ""
   (if (car lists)
@@ -29,38 +54,21 @@
 (defun rtos/interrupt-template (dev)
   ""
   (cond ((string-match "UART" dev)
-	 (format "unsigned short i;
-    hw_notification notification;
-    /* TODO: determine which bit to clear:  */
-    unsigned long look_at_me = UARTIntStatus();
-    /* UARTIntClear(%s_BASE, ); */
-
-    while(UARTCharsAvail(%s_BASE)) {
-
-	/* Notify every subscribed task of each incoming character
-	 * (but schedule them for later so we can return from this ISR
-	 * asap). */
-	notification._char = uart_get_char();
-
-	/* TODO: schedule this thread instead of running it immediately */
-	hw_notify(HW_UART, %s_BASE, notification);
-    }" dev dev dev))
-
+	 (format (cdr (assoc 'uart rtos/interrupt-template-alist)) dev dev dev))
 	((string-match "TIMER" dev)
-	 (format "TimerIntClear(%s_BASE, TIMER_TIMA_TIMEOUT);
-    hw_notification notification;
-    notification._int = 1;
-    hw_notify(HW_TIMER, %s_BASE, notification);" dev dev))))
+	 (format (cdr (assoc 'timer rtos/interrupt-template-alist)) dev dev))))
 
 ;;;###autoload
 (defun rtos/generate-interrupts()
   ""
   (interactive)
-  (mapcar (lambda (data)
+  (save-excursion
+    (mapcar (lambda (data)
 	    (let* ((device (format (cadr data) (car data)))
 		   (template (rtos/interrupt-template device)))
 	      (rtos/generate-interrupt-handler device template)))
-	  (combinations '(0 1 2) '("UART%s" "TIMER%sA"))))
+	    (combinations '(0 1 2) '("UART%s" "TIMER%sA"))))
+  (kill-line))
 
 (provide 'rtos-interrupt-generator)
 
