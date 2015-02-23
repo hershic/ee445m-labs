3ebf9b06e0f1ec70122d7ec97dbdb174361a59a0
Eric Crosson <esc@ericcrosson.com>
2015-02-23 08:56:05 -0600

    interrupt generator simplified
    with data separated from code

--- bin/lisp/rtos-interrupt-generator.el
+++ bin/lisp/rtos-interrupt-generator.el
@@ -8,31 +8,17 @@
 
 ;;; Code:
 
-(defun read-lines (file)
-  "Return FILE's file content."
-  (with-temp-buffer
-    (insert-file-contents file)
-    (buffer-string)))
-
-;; todo: pull from a file to avoid this nonsense, use a macro to make it clean
-(defvar rtos/interrupt-template-alist nil
-  "Contains (DEVICE . TEMPLATE) of interrupt handlers.")
-(setq rtos/interrupt-template-alist
-      ;; TODO: insert '    ' after new lines
-      `((uart . ,(read-lines "templates/uart.c"))
-	(timer . ,(read-lines "templates/timer.c"))))
-
-(defvar rtos/interrupt-devices nil
-  "Templates of interruptable device names on the TM4C123G Cortex
-  M4.")
-(setq rtos/interrupt-devices '("UART%s" "TIMER%s"))
-
 (defvar rtos/interrupt-channels nil
   "Templates of interruptable device channel numbers on the
   TM4C123G Cortex M4.")
 (setq rtos/interrupt-channels '(0 1 2))
 
-(defun combinations (&rest lists)
+(defvar rtos/interrupt-devices nil
+  "Templates of interruptable device device numbers on the
+  TM4C123G Cortex M4.")
+(setq rtos/interrupt-devices '(uart timer))
+
+(defun rtos/combinations (&rest lists)
   "Return a list of all possible combinations of the elements of LISTS."
   (if (car lists)
       (mapcan (lambda (inner-val)
@@ -40,25 +26,21 @@
                           (cons outer-val
                                 inner-val))
                         (car lists)))
-              (apply #'combinations (cdr lists)))
+              (apply #'rtos/combinations (cdr lists)))
     (list nil)))
 
-(defun rtos/generate-interrupt-handler(device template)
-  "Use information about DEVICE to return a populated device TEMPLATE."
-  (let ((body (format
-	       (concat "\nvoid %s_Handler(void) {\n\n    " template "\n}\n")
-	       device)))
-    (insert body)))
-
-(defun rtos/interrupt-template (dev)
-  "Populate and return an interrupt service routine template for
-device DEV."
-  (cond ((string-match "UART" dev)
-	 ;; TODO: more meta processing, remove repeated dev calls by
-	 ;; counting occurances of '%s' in the template alist
-	 (format (cdr (assoc 'uart rtos/interrupt-template-alist)) dev dev dev))
-	((string-match "TIMER" dev)
-	 (format (cdr (assoc 'timer rtos/interrupt-template-alist)) dev dev))))
+(defun rtos/isr-template (file sub)
+  "Return a list of lines of a file at FILE (symbol) with format
+strings %s substituted with (concat FILE SUB)."
+  (let ((c-tab "    ")
+	(interface (symbol-name file)))
+    (with-temp-buffer
+      (insert-file-contents (format "templates/%s.c" interface))
+      (format "%s%s"
+	      c-tab
+	      (mapconcat 'identity
+			 (split-string (buffer-string) "\n" t)
+		 (format "\n%s" c-tab))))))
 
 ;;;###autoload
 (defun rtos/generate-interrupts()
@@ -67,12 +49,25 @@ device and channel number in `rtos/interrupt-devices' and
 `rtos/interrupt-channels', respectively."
   (interactive)
   (save-excursion
-    (mapcar (lambda (data)
-	    (let* ((device (format (cadr data) (car data)))
-		   (template (rtos/interrupt-template device)))
-	      (rtos/generate-interrupt-handler device template)))
-	    (combinations rtos/interrupt-channels rtos/interrupt-devices)))
+    (mapcar (lambda (template)
+	      (let* ((device (cadr template))
+		     (id     (car template))
+		     (isr-handler (upcase (symbol-name device)))
+		     (fn-body (format "%s\n" (rtos/isr-template device id))))
+		;; todo wrap in fn prototype and interpolate
+		(insert
+		 (replace-regexp-in-string
+		  "%s"
+		  (format "%s%d%s" isr-handler id (if (eq device 'timer) "A" ""))
+		  (format "%s%s"
+			 "\nvoid %s_Handler(void) {\n\n"
+			 (replace-regexp-in-string
+			  "%s"
+			  (format "%s%d" isr-handler id)
+			  (format "%s}\n" fn-body)))))))
+	    (rtos/combinations rtos/interrupt-channels rtos/interrupt-devices)))
   (kill-line))
+
 
 (provide 'rtos-interrupt-generator)
 
