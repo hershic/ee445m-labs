479eaae510b50448914497e8bfa9dfe783b531f5
Hershal Bhave <hershal.bhave@gmail.com>
2015-02-26 02:52:56 -0600

    test-timer solves lab2.2.1

--- board-progs/test-timer/test-timer.c
+++ board-progs/test-timer/test-timer.c
@@ -14,7 +14,7 @@
 
 /* Driverlib Includes */
 #include "driverlib/debug.h"
-#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
 #include "driverlib/timer.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/pin_map.h"
@@ -28,21 +28,36 @@
 #define HEARTBEAT_MODAL
 #include "libheart/heartbeat.h"
 
-sem_t sem;
+volatile uint32_t sem;
 uint32_t interrupt_counter;
 uint32_t wait_counter;
 
 muscle_t red_muscle;
 muscle_t blue_muscle;
 
+void (*timer_task)(void);   // user function
+
+void timer_set_task(void (*task)(void)) {
+    timer_task = task;
+}
+
+void timer_release() {
+    sem = 0xdeadfeed;
+    /* sem_post(&sem); */
+}
+
 void TIMER0A_Handler(void) {
 
-    /* IntMasterDisable(); */
     TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-    heart_toggle_modal(&red_muscle);
+    (*timer_task)();
+
+    /* doesn't work */
+    /* heart_toggle_modal(&red_muscle); */
+
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1,
+                 GPIO_PIN_1 ^ GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1));
+
     ++interrupt_counter;
-    sem_post(&sem);
-    /* IntMasterEnable(); */
 }
 
 /*! Accept input on UART 0, and parrot input back out to UART 0.
@@ -61,7 +76,10 @@ int main(void) {
 
     heart_init();
 
-    sem_init(&sem, 0);
+    /* sem_init(&sem, 0); */
+    sem = false;
+
+    timer_set_task(&timer_release);
 
     timer_metadata_init(TIMER0_BASE, 2 Hz, INT_TIMER0A, TIMER_CFG_PERIODIC);
 
@@ -76,13 +94,17 @@ int main(void) {
 
     heart_hew_muscle(red_muscle, GPIO_PORTF_BASE, HEART_RED);
     heart_hew_muscle(blue_muscle, GPIO_PORTF_BASE, HEART_BLUE);
+    heart_init_(red_muscle.base, red_muscle.pin);
+    heart_init_(blue_muscle.base, blue_muscle.pin);
 
     while (1) {
-        while(!sem) {
-            heart_beat();
-            heart_toggle_modal(&blue_muscle);
-        }
+        while (!sem) {}
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2,
+                     GPIO_PIN_2 ^ GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_2));
+
+        /* heart_toggle_modal(&blue_muscle); */
         ++wait_counter;
+        sem = 0;
     }
     /* postpone_death(); */
 }
