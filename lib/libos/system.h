3e18b7b20652c5fa88ff8fdd0a81bd9ab3e8052f
Eric Crosson <esc@ericcrosson.com>
2015-02-26 20:50:48 -0600

    system.h wrapped in doxygen group

--- lib/libos/system.h
+++ lib/libos/system.h
@@ -6,20 +6,24 @@
 #include <stdbool.h>
 #include "libstd/nexus.h"
 
-/** Maximum length of a callable command. */
+/*! \addtogroup System
+ * @{
+ */
+
+/*! Maximum length of a callable command. */
 #define SYSTEM_MAX_COMMAND_NAME_LENGTH 16
 
-/** Maximum number of commands supported by the system. */
+/*! Maximum number of commands supported by the system. */
 #define SYSTEM_MAX_COMMANDS            16
 
-/** Iterator optimized for size that is guaranteed to be able to
+/*! Iterator optimized for size that is guaranteed to be able to
  * iterate over \SYSTEM_COMMANDS. */
 typedef uint8_t  system_iterator;
 
-/** Type definition of an exit code. */
+/*! Type definition of an exit code. */
 typedef uint8_t exit_status_t;
 
-/** Struct containing properties of a system command. */
+/*! Struct containing properties of a system command. */
 struct system_command;
 typedef struct system_command {
 
@@ -32,28 +36,32 @@ typedef struct system_command {
     struct system_command* prev;
 } system_command;
 
-/** Initialize internal data structures.
+/*! Initialize internal data structures.
  * \note Ensure this method is called before any commands are registered. */
 void system_init();
 
-/** Register a command for later execution.
+/*! Register a command for later execution.
  * \returns True if command was successfully registered.
  */
 bool system_register_command(const char*, int(*)());
 
-/** Remove a command from the index of executable system commands.
+/*! Remove a command from the index of executable system commands.
  * \returns True if command was successfully deregistered.
  */
 bool system_deregister_command(const char*);
 
-/** Execute a system command.
+/*! Execute a system command.
  * \returns The exit code of the executed command.
  * \bug Currently argument passing is not supported.
  */
 exit_status_t system_exec(const char*, const char**);
 
-/** For internal use only. This function returns a pointer to the
+/*! For internal use only. This function returns a pointer to the
  * \system_command registered to the command name \command_name. */
 system_command* _system_command_from_name(const char* command_name);
 
 #endif
+
+/*! End doxygen group
+ * @}
+ */
