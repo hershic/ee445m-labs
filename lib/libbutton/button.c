ceb4fe56bc5432fa2f2ebcff16de96b5e91eee0e
Hershal Bhave <hershal.bhave@gmail.com>
2015-02-28 10:30:00 +0000

    separate button detection works

--- lib/libbutton/button.c
+++ lib/libbutton/button.c
@@ -9,6 +9,8 @@
 
 #include "inc/hw_memmap.h"
 #include "inc/hw_ints.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
 
 #include <stdlib.h>
 
@@ -35,8 +37,13 @@
 void button_init(hw_metadata metadata) {
 
     GPIODirModeSet(GPIO_PORTF_BASE, metadata.button.pin, GPIO_DIR_MODE_IN);
+
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_CR) = 0x01;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
+
     GPIOPadConfigSet(GPIO_PORTF_BASE, metadata.button.pin,
-		     GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+                     GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
     GPIOIntEnable(GPIO_PORTF_BASE, metadata.button.pin);
 }
 
@@ -44,8 +51,8 @@ void button_set_interrupt(hw_metadata metadata) {
 
     if (metadata.button.interrupt) {
         GPIOIntTypeSet(metadata.button.base,
-		       metadata.button.pin,
-		       metadata.button.interrupt);
+                       metadata.button.pin,
+                       metadata.button.interrupt);
         IntEnable(INT_GPIOF_TM4C123);
     } else {
         IntDisable(INT_GPIOF_TM4C123);
