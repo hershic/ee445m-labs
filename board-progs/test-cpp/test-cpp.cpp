0393705c1ac86e3c841cc40fa16ffde3e9f64a12
Hershal Bhave <hershal.bhave@gmail.com>
2015-03-18 00:39:12 -0500

    removed kludge

--- board-progs/test-cpp/test-cpp.cpp
+++ board-progs/test-cpp/test-cpp.cpp
@@ -26,14 +26,3 @@ int main(void) {
         blink.toggle(RED);
     }
 }
-
-/* extern "C" void* malloc(size_t) { */
-/*     return (void*) 0; */
-/* } */
-
-/* extern "C" void free(void*) { } */
-
-/* void* operator new(size_t size) throw() { return malloc(size); } */
-/* void  operator delete(void* p) { free(p); } */
-/* extern "C" int __aeabi_atexit(void* object, void (*detructor)(void*), */
-/*                               void *dso_handle) { return 0; } */
