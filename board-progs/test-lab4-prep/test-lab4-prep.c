6022cb2f704217a78e79f08052dc565565383312
Hershal Bhave <hershal.bhave@gmail.com>
2015-03-22 23:21:35 -0500

    convolution works

--- board-progs/test-lab4-prep/test-lab4-prep.c
+++ board-progs/test-lab4-prep/test-lab4-prep.c
@@ -30,9 +30,22 @@
 
 #include "libheart/heartbeat.h"
 
+#define filter_length 51
+#define signal_length 51
+
+const int32_t h[filter_length]={4,-1,-8,-14,-16,-10,-1,6,5,-3,-13,
+                                 -15,-8,3,5,-5,-20,-25,-8,25,46,26,-49,-159,-257, 984,
+                                 -257,-159,-49,26,46,25,-8,-25,-20,-5,5,3,-8,
+                                 -15,-13,-3,5,6,-1,-10,-16,-14,-8,-1,4};
+
+int32_t adc_raw_data[signal_length];
+int32_t adc_filtered_data[filter_length+signal_length-1];
+
 extern semaphore_t HW_ADC_SEQ2_SEM;
 extern semaphore_t HW_BUTTON_RAW_SEM;
-extern uint32_t ADC0_SEQ2_SAMPLES[4];
+semaphore_t FILTERED_DATA_AVAIL;
+extern int32_t ADC0_SEQ2_SAMPLES[4];
+
 uint32_t red_work = 0;
 uint32_t blue_work = 0;
 uint32_t green_work = 0;
@@ -46,6 +59,23 @@ volatile semaphore_t button_debounced_new_data;
 
 int8_t plot_en;
 int8_t print_sample_raw;
+int8_t print_sample_filtered;
+
+void convolve(int32_t *x, int32_t *h, int32_t *y, int32_t filter_len, int32_t signal_len) {
+    uint32_t i, j;
+    uint32_t jmin, jmax;
+    for (i=0; i<(signal_length + filter_length-1); ++i) {
+        y[i] = 0;
+
+        jmin = (i >= signal_length - 1) ? i - (signal_length - 1) : 0;
+        jmax = (i < filter_length - 1) ? i : filter_length - 1;
+
+        for(j = jmin; j<=jmax; ++j) {
+            y[i]+= (h[j] * x[i-j]);
+        }
+        y[i] /= 256;
+    }
+}
 
 void led_blink_red() {
     while (1) {
@@ -110,82 +140,58 @@ void display_all_adc_data() {
     ST7735_PlotClear(0, 4095);
     plot_en = 1;
     print_sample_raw = 0;
+    print_sample_filtered = 0;
+    i = 0;
 
     while (1) {
-        sem_guard(HW_ADC_SEQ2_SEM && plot_en) {
-            sem_take(HW_ADC_SEQ2_SEM);
+        sem_guard(FILTERED_DATA_AVAIL) {
+            if(plot_en) {
+                sem_take(FILTERED_DATA_AVAIL);
 
-            fixed_4_digit_i2s(string_buf, ADC0_SEQ2_SAMPLES[0]);
-            ST7735_DrawString(1, 1, string_buf, ST7735_YELLOW);
+                fixed_4_digit_i2s(string_buf, adc_filtered_data[50]);
+                ST7735_DrawString(1, 1, string_buf, ST7735_YELLOW);
 
-            ST7735_PlotLine(ADC0_SEQ2_SAMPLES[0]);
-            if (ST7735_PlotNext()) {
-                ST7735_PlotClear(0, 4095);
-            }
-            if (print_sample_raw) {
-                poor_mans_uart_send_string(string_buf);
-                print_sample_raw = 0;
+                ST7735_PlotLine(adc_filtered_data[0]);
+                if (ST7735_PlotNext()) {
+                    ST7735_PlotClear(0, 4095);
+                }
+                if (print_sample_raw) {
+                    fixed_4_digit_i2s(string_buf, ADC0_SEQ2_SAMPLES[50]);
+                    poor_mans_uart_send_string("raw: ");
+                    poor_mans_uart_send_string(string_buf);
+                    poor_mans_uart_send_string("\r\n");
+                    print_sample_raw = 0;
+                }
+                if (print_sample_filtered) {
+                    poor_mans_uart_send_string("filtered: ");
+                    poor_mans_uart_send_string(string_buf);
+                    poor_mans_uart_send_string("\r\n");
+                    print_sample_filtered = 0;
+                }
             }
         }
         os_surrender_context();
     }
 }
 
-void display_digital_adc_data() {
+void filter() {
 
-    int8_t i;
-    uint8_t string_buf[5];
-
-    while (1) {
-        sem_guard(HW_ADC_SEQ2_SEM) {
-            sem_take(HW_ADC_SEQ2_SEM);
-
-            for (i=0; i<4; ++i) {
-                fixed_4_digit_i2s(string_buf, ADC0_SEQ2_SAMPLES[i]);
-                ST7735_DrawString(2, 2+i, string_buf, ST7735_YELLOW);
-            }
-        }
-        os_surrender_context();
-    }
-}
-
-void display_analog_adc_data() {
-
-    int8_t i;
-    char string_buf[5];
-    ST7735_PlotClear(0, 4095);
+    int32_t i=0;
 
     while (1) {
         sem_guard(HW_ADC_SEQ2_SEM) {
             sem_take(HW_ADC_SEQ2_SEM);
+            adc_raw_data[i++] = ADC0_SEQ2_SAMPLES[0];
 
-            fixed_4_digit_i2s(string_buf, ADC0_SEQ2_SAMPLES[0]);
-            ST7735_DrawString(1, 1, string_buf, ST7735_YELLOW);
-
-            ST7735_PlotLine(ADC0_SEQ2_SAMPLES[0]);
-            if (ST7735_PlotNext()) {
-                ST7735_PlotClear(0, 4095);
+            if (i >= signal_length) {
+                i = 0;
+                convolve(adc_raw_data, h, adc_filtered_data, filter_length, signal_length);
+                sem_post(FILTERED_DATA_AVAIL);
             }
         }
         os_surrender_context();
     }
 }
-
-/* void button_debounce_end(notification button_notification) { */
-
-/*     button_debounced_mailbox = GPIOPinRead(GPIO_PORTF_BASE, BUTTONS_BOTH); */
-/*     sem_post(button_debounced_new_data); */
-/* } */
-
-/* /\* what the btn handler calls *\/ */
-/* void button_debounce_start(notification button_notification) { */
-
-/*     /\* button_debounced_wtf = GPIOPinRead(GPIO_PORTF_BASE, BUTTONS_BOTH); *\/ */
-/*     timer_metadata_init(TIMER0_BASE, 10 Hz, INT_TIMER0A, TIMER_CFG_ONE_SHOT); */
-/*     hw_channel_init(HW_TIMER, timer_metadata); */
-/*     hw_subscribe_single_shot(HW_TIMER, timer_metadata, */
-/*                              button_debounce_end); */
-/* } */
 
 void button_debounce_daemon() {
 
@@ -231,6 +237,32 @@ int sample_raw() {
     print_sample_raw = 1;
 }
 
+int sample_filtered() {
+    print_sample_filtered = 1;
+}
+
+int sample_both() {
+    print_sample_filtered = 1;
+    print_sample_raw = 1;
+}
+
+void simulate_adc() {
+    int8_t j;
+    int32_t i;
+
+    while (1) {
+        ++j;
+
+        if (j > 10) {
+            sem_post(HW_ADC_SEQ2_SEM);
+            ADC0_SEQ2_SAMPLES[0] = i & 4095;
+            ++i;
+            j = 0;
+        }
+        os_suspend();
+    }
+}
+
 int main(void) {
 
     hw_metadata metadata;
@@ -263,14 +295,14 @@ int main(void) {
     metadata.adc.trigger_metadata.timer.interrupt = INT_TIMER1A;
     metadata.adc.trigger_metadata.timer.periodic = TIMER_CFG_PERIODIC;
 
-    adc_init(metadata);
-    adc_channel_init(metadata);
-    adc_interrupt_init(metadata);
+    /* adc_init(metadata); */
+    /* adc_channel_init(metadata); */
+    /* adc_interrupt_init(metadata); */
     /* end adc init */
 
     /* begin timer init for button debouncer */
-    timer_metadata_init(TIMER0_BASE, 10 Hz, INT_TIMER0A, TIMER_CFG_ONE_SHOT);
-    hw_driver_init(HW_TIMER, timer_metadata);
+    /* timer_metadata_init(TIMER0_BASE, 10 Hz, INT_TIMER0A, TIMER_CFG_ONE_SHOT); */
+    /* hw_driver_init(HW_TIMER, timer_metadata); */
     /* end timer init for button debouncer */
 
     /* button init */
@@ -286,15 +318,19 @@ int main(void) {
     /* end button init */
 
     os_threading_init();
-    schedule(led_blink_red, 100 Hz, DL_SOFT);
+    /* schedule(led_blink_red, 100 Hz, DL_SOFT); */
     schedule(display_all_adc_data, 200 Hz, DL_SOFT);
     schedule(hw_daemon, 100 Hz, DL_SOFT);
     schedule(button_debounce_daemon, 100 Hz, DL_SOFT);
+    schedule(filter, 100 Hz, DL_SOFT);
+    schedule(simulate_adc, 100 Hz, DL_SOFT);
 
     system_init();
     system_register_command((const char*) "plot_on", plot_on);
     system_register_command((const char*) "plot_off", plot_off);
     system_register_command((const char*) "sample_raw", sample_raw);
+    system_register_command((const char*) "sample_filtered", sample_filtered);
+    system_register_command((const char*) "sample_both", sample_both);
 
     /* Initialize hardware devices */
     uart_metadata_init(UART_DEFAULT_BAUD_RATE, UART0_BASE, INT_UART0);
@@ -303,7 +339,6 @@ int main(void) {
     /* Initialize the shell and the system it interacts with */
     shell_spawn();
 
-    IntMasterEnable();
     os_launch();
 
     while (1) {
