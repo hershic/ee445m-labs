d66536e849c540b6734f5e9c4d0b97f2c6f9a539
Hershal Bhave <hershal.bhave@gmail.com>
2015-04-03 07:17:51 -0500

    added valvano code for lab5

---
+++ board-progs/lab5-valvano/efile.h
@@ -0,0 +1,94 @@
+// filename ************** eFile.h *****************************
+// Middle-level routines to implement a solid-state disk 
+// Jonathan W. Valvano 3/16/11
+
+//---------- eFile_Init-----------------
+// Activate the file system, without formating
+// Input: none
+// Output: 0 if successful and 1 on failure (already initialized)
+// since this program initializes the disk, it must run with 
+//    the disk periodic task operating
+int eFile_Init(void); // initialize file system
+
+//---------- eFile_Format-----------------
+// Erase all files, create blank directory, initialize free space manager
+// Input: none
+// Output: 0 if successful and 1 on failure (e.g., trouble writing to flash)
+int eFile_Format(void); // erase disk, add format
+
+//---------- eFile_Create-----------------
+// Create a new, empty file with one allocated block
+// Input: file name is an ASCII string up to seven characters 
+// Output: 0 if successful and 1 on failure (e.g., trouble writing to flash)
+int eFile_Create( char name[]);  // create new file, make it empty 
+
+
+//---------- eFile_WOpen-----------------
+// Open the file, read into RAM last block
+// Input: file name is a single ASCII letter
+// Output: 0 if successful and 1 on failure (e.g., trouble writing to flash)
+int eFile_WOpen(char name[]);      // open a file for writing 
+
+//---------- eFile_Write-----------------
+// save at end of the open file
+// Input: data to be saved
+// Output: 0 if successful and 1 on failure (e.g., trouble writing to flash)
+int eFile_Write( char data);  
+
+//---------- eFile_Close-----------------
+// Deactivate the file system
+// Input: none
+// Output: 0 if successful and 1 on failure (not currently open)
+int eFile_Close(void); 
+
+
+//---------- eFile_WClose-----------------
+// close the file, left disk in a state power can be removed
+// Input: none
+// Output: 0 if successful and 1 on failure (e.g., trouble writing to flash)
+int eFile_WClose(void); // close the file for writing
+
+//---------- eFile_ROpen-----------------
+// Open the file, read first block into RAM 
+// Input: file name is a single ASCII letter
+// Output: 0 if successful and 1 on failure (e.g., trouble read to flash)
+int eFile_ROpen( char name[]);      // open a file for reading 
+   
+//---------- eFile_ReadNext-----------------
+// retreive data from open file
+// Input: none
+// Output: return by reference data
+//         0 if successful and 1 on failure (e.g., end of file)
+int eFile_ReadNext( char *pt);       // get next byte 
+                              
+//---------- eFile_RClose-----------------
+// close the reading file
+// Input: none
+// Output: 0 if successful and 1 on failure (e.g., wasn't open)
+int eFile_RClose(void); // close the file for writing
+
+//---------- eFile_Directory-----------------
+// Display the directory with filenames and sizes
+// Input: pointer to a function that outputs ASCII characters to display
+// Output: characters returned by reference
+//         0 if successful and 1 on failure (e.g., trouble reading from flash)
+int eFile_Directory(void(*fp)(unsigned char));   
+
+//---------- eFile_Delete-----------------
+// delete this file
+// Input: file name is a single ASCII letter
+// Output: 0 if successful and 1 on failure (e.g., trouble writing to flash)
+int eFile_Delete( char name[]);  // remove this file 
+
+//---------- eFile_RedirectToFile-----------------
+// open a file for writing 
+// Input: file name is a single ASCII letter
+// stream printf data into file
+// Output: 0 if successful and 1 on failure (e.g., trouble read/write to flash)
+int eFile_RedirectToFile(char *name);
+
+//---------- eFile_EndRedirectToFile-----------------
+// close the previously open file
+// redirect printf data back to UART
+// Output: 0 if successful and 1 on failure (e.g., wasn't open)
+int eFile_EndRedirectToFile(void);
