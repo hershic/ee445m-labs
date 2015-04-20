86f7c801574e3142aa82e82f28dc02c31b2ffb54
Eric Crosson <esc@ericcrosson.com>
2015-04-20 17:30:56 -0500

    hello semaphorepp

---
+++ board-progs/test-cpp/semaphorepp.cpp
@@ -0,0 +1,48 @@
+#include "semaphorepp.hpp"
+
+semaphore::semaphore() {
+
+    reset();
+}
+
+void semaphore::reset() {
+
+    int32_t status = StartCritical();
+    value = 0;
+    EndCritical(status);
+}
+
+void semaphore::wait() {
+
+    while(blocked()) {
+        os_surrender_context();
+    }
+    int32_t status = StartCritical();
+    --value;
+    EndCritical(status);
+}
+
+void semaphore::post() {
+
+    int32_t status = StartCritical();
+    value++;
+    EndCritical(status);
+}
+
+bool semaphore::guard() {
+
+    return value > 0;
+}
+
+bool semaphore::blocked() {
+
+    return value <= 0;
+}
+
+/*! \warning: you had better be guaranteed permission to take this */
+void semaphore::take() {
+
+    int32_t status = StartCritical();
+    value--;
+    EndCritical(status);
+}
---
+++ board-progs/test-cpp/semaphorepp.hpp
@@ -0,0 +1,40 @@
+/* -*- mode: c; c-basic-offset: 4; -*- */
+/* Created by Hershal Bhave and Eric Crosson 2015-03-08 */
+/* Revision history: Look in Git FGT */
+#ifndef __thread_structrues__
+#define __thread_structrues__
+
+/*! \addtogroup semaphore
+ * @{
+ */
+
+class semaphore {
+private:
+    int16_t value;
+public:
+    semaphore();
+
+    /*! Increment the semaphore. */
+    void post(void);
+
+    /*! Wait on a semaphore. */
+    void wait(void);
+
+    /*! Take the semaphore */
+    void take(void);
+
+    /*! True if the semaphore is ready to take. */
+    bool guard(void);
+
+    /*! True if the semaphore is blocked. */
+    bool blocked(void);
+
+    /*! Reset the semaphore. */
+    void reset(void);
+}
+
+#endif
+
+/* End Doxygen group
+    * @}
+     */
