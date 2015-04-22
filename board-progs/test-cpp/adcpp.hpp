88f756a382e771828bcbec1cb5d01797834bc2c8
Hershal Bhave <hershal.bhave@gmail.com>
2015-04-22 16:41:46 -0500

    adcpp: made attributes public

--- board-progs/test-cpp/adcpp.hpp
+++ board-progs/test-cpp/adcpp.hpp
@@ -10,10 +10,10 @@ typedef uint32_t memory_address_t;
 
 class adc {
 private:
-
     static const uint8_t default_priority = 0;
     static const uint8_t max_num_sequencer_steps = 8;
 
+public:
     memory_address_t base;
     uint8_t sequencer;
     uint8_t channel_counter;
@@ -21,7 +21,7 @@ private:
     uint8_t trigger_source;
 
     uint32_t producer_index;
-public:
+
     /*! Initialize adc. */
     adc();
     adc(memory_address_t adc_base, uint8_t adc_trigger_source, uint8_t adc_sequencer);
