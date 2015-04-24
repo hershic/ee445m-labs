d68e72a09df518662201846cff1a83d81ac1f9b3
Hershal Bhave <hershal.bhave@gmail.com>
2015-04-24 18:51:59 -0500

    motor: pwm works with driverlib

--- board-progs/test-cpp/motorpp.cpp
+++ board-progs/test-cpp/motorpp.cpp
@@ -73,12 +73,6 @@ void motor::set(percent_t percent_full_speed) {
 
 void motor::motor_init() {
 
-    /* GPIO_PORTE_DIR_R |= 0x01;    // make PE3-0 output heartbeats */
-    /* GPIO_PORTE_AFSEL_R &= ~0x01;   // disable alt funct on PE3-0 */
-    /* GPIO_PORTE_DEN_R |= 0x01;     // enable digital I/O on PE3-0 */
-    /* GPIO_PORTE_PCTL_R = ~0x0000FFFF; */
-    /* GPIO_PORTE_AMSEL_R &= ~0x01;      // disable analog functionality on PF */
-
     direction = motor_installed_backwards ? BACKWARD : FORWARD;
 
     pwm_init();
@@ -94,11 +88,26 @@ void motor::pwm_init() {
 
     uint32_t status = StartCritical();
     ctlsys::enable_periph(pwm_base);
+    ctlsys::enable_periph(GPIO_PORTB_BASE);
 
+    SysCtlPWMClockSet(SYSCTL_PWMDIV_64);
     PWMGenConfigure(pwm_base, pwm_gen, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
     PWMGenPeriodSet(pwm_base, pwm_gen, DEFAULT_PWM_PERIOD);
     PWMPulseWidthSet(pwm_base, pwm_out, 75*DEFAULT_PWM_PERIOD/100);
     PWMGenEnable(pwm_base, pwm_gen);
+
+    switch(pwm_out) {
+    case PWM_OUT_0:
+        GPIOPinConfigure(GPIO_PB6_M0PWM0);
+        GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_6);
+        break;
+    case PWM_OUT_1:
+        GPIOPinConfigure(GPIO_PB7_M0PWM1);
+        GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_7);
+        break;
+    default:
+        while(1) {}
+    }
 
     /* Enable the outputs. */
     PWMOutputState(pwm_base, (PWM_OUT_0_BIT | PWM_OUT_1_BIT), true);
