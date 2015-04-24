77539314aeb59537303f244d1a8892d583d7db86
Eric Crosson <esc@ericcrosson.com>
2015-04-23 21:57:12 -0500

    code cleanup

--- board-progs/test-cpp/test-cpp.cpp
+++ board-progs/test-cpp/test-cpp.cpp
@@ -29,10 +29,10 @@
 #include "driverlib/uart.h"
 
 #define thread(x)                   \
-    while(1) {                      \
-        x                           \
-            os_surrender_context(); \
-    }
+    do {                            \
+        x;                          \
+        os_surrender_context();     \
+    } while(true)
 
 blinker blink;
 timer timer0a;
@@ -46,7 +46,7 @@ motor motor0, motor1;
 drive drive0;
 
 #define can_data_length 8
-#define can_msg_id 1
+const uint32_t can_msg_id = 1;
 const bool can_sender = false;
 can can0;
 semaphore can_recv_sem;
@@ -60,43 +60,39 @@ uint32_t blink_count_red = 0;
 uint32_t blink_count_green = 0;
 uint32_t blink_count_blue = 0;
 
-void thread_0() {
+void thread_blink_red() {
+
+    thread (
+            blink.toggle(PIN_RED);
+            ++blink_count_red;
+        );
+}
+
+void thread_blink_blue() {
 
     thread (
             blink.toggle(PIN_BLUE);
             ++blink_count_blue;
-            )
-        }
+        );
+}
 
-void thread_1() {
+void thread_blink_green() {
 
     thread (
             blink.toggle(PIN_GREEN);
             ++blink_count_green;
-            )
-        }
+        );
+}
 
 void thread_uart_update() {
 
     thread (
-            uart0.atomic_printf("%d\n\r", blink_count_blue);
-            )
-        }
-
-void shell_handler() {
-
-    while(1) {
-        if(UART0_RX_SEM.guard()) {
-            UART0_RX_SEM.take();
-
-            bool ok;
-            char recv = UART0_RX_BUFFER.get(ok);
-
-            if(ok) { shell0.accept(recv); }
-        }
-        os_surrender_context();
-    }
+            uart0.atomic_printf("%x %x %x\n\r", blink_count_red,
+                                blink_count_blue, blink_count_green);
+        );
 }
+
+extern "C" void __cxa_pure_virtual() { while (1) {} }
 
 extern "C" void Timer0A_Handler() {
     timer0a.ack();
@@ -148,16 +144,13 @@ extern "C" void ADC0Seq0_Handler(void) {
 
 extern "C" void CAN0_Handler(void) {
 
-    // Read the CAN interrupt status to find the cause of the interrupt
     uint32_t message_id = can0.ack();
 
     switch(message_id) {
     case 1:
-        // Since the message was sent, clear any error flags.
         can_recv_sem.post();
         break;
     default:
-        /* will this ever hang? put your money in now */
         /* while(1) {} */
         break;
     }
@@ -170,7 +163,9 @@ void can_handler(void) {
             can_recv_sem.take();
 
             can0.mailbox(can_data);
-            uart0.atomic_printf("Received CAN data: %0X %0X\n\r", can_data[0], can_data[1]);
+            uart0.atomic_printf("Received CAN data: %0X %0X %0X %0X %0X %0X %0X %0X ",
+                                can_data[0], can_data[1], can_data[3], can_data[4],
+                                can_data[4], can_data[5], can_data[6], can_data[7]);
         }
         os_surrender_context();
     }
@@ -185,7 +180,20 @@ void can_transmitter(void) {
     }
 }
 
-extern "C" void __cxa_pure_virtual() { while (1) {} }
+void shell_handler() {
+
+    while(1) {
+        if(UART0_RX_SEM.guard()) {
+            UART0_RX_SEM.take();
+
+            bool ok;
+            char recv = UART0_RX_BUFFER.get(ok);
+
+            if(ok) { shell0.accept(recv); }
+        }
+        os_surrender_context();
+    }
+}
 
 void motor_control(void) {
     while(1) {
@@ -259,8 +267,9 @@ int main(void) {
 
     os_threading_init();
     /* schedule(motor_control, 200); */
-    schedule(thread_1, 200);
-    schedule(thread_0, 200);
+    schedule(thread_blink_red, 200);
+    schedule(thread_blink_blue, 200);
+    schedule(thread_blink_green, 200);
     schedule(shell_handler, 200);
     if(can_sender) {
         schedule(can_transmitter, 200);
