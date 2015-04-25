246d4854e7b83976e8ae4922773d010e4330872a
Eric Crosson <esc@ericcrosson.com>
2015-04-25 01:19:31 -0500

    scheduled motor control

--- board-progs/test-cpp/test-cpp.cpp
+++ board-progs/test-cpp/test-cpp.cpp
@@ -251,9 +251,12 @@ int main(void) {
     uart0.printf("\n\rWelcome to RRTOS v0\n\r");
     shell0 = shell(&uart0, &motor_start, &motor_stop);
 
-    can_recv_sem = semaphore();
     can0 = can(CAN0_BASE, INT_CAN0, can_sender, can_data_length);
-    if(can_sender) { can_prepare_dummy_data(); }
+    if(can_sender) {
+        can_prepare_dummy_data();
+    } else {
+        can_recv_sem = semaphore();
+    }
 
     motor_start = semaphore();
     motor_stop = semaphore();
@@ -261,10 +264,10 @@ int main(void) {
     motor1 = motor(GPIO_PORTA_BASE, GPIO_PIN_7, PWM0_BASE, PWM_GEN_0, PWM_OUT_1);
     motor1.set_motor_installed_backwards(true);
     drive0 = drive(&motor0, &motor1);
-    /* drive0.forward(20); */
+    drive0.forward(50);
 
     os_threading_init();
-    /* schedule(motor_control, 200); */
+    schedule(motor_control, 200);
     schedule(thread_blink_red, 200);
     schedule(thread_blink_blue, 200);
     schedule(thread_blink_green, 200);
@@ -274,7 +277,6 @@ int main(void) {
     } else {
         schedule(can_handler, 200);
     }
-    /* schedule(thread_uart_update, 1000000); */
     os_launch();
 }
 
