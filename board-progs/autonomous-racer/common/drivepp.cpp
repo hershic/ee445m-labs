42c2cc69449fbda2b94233b11b27d84e13821efc
Hershal Bhave <hershal.bhave@gmail.com>
2015-04-27 20:55:47 -0500

    drive: *HACK* follow a left-side wall

--- board-progs/autonomous-racer/common/drivepp.cpp
+++ board-progs/autonomous-racer/common/drivepp.cpp
@@ -42,6 +42,16 @@ void drive::set(percent_t speed, Direction dir) {
     right->set(speed, dir);
 }
 
+int32_t clamp(int32_t value, int32_t min, int32_t max) {
+    if (value > max) {
+        return max;
+    } else if (value < min) {
+        return min;
+    } else {
+        return value;
+    }
+}
+
 void drive::steer(uint32_t left_sens, uint32_t left_front_sens,
                   uint32_t right_sens, uint32_t right_front_sens,
                   uint32_t back_sens) {
@@ -52,8 +62,11 @@ void drive::steer(uint32_t left_sens, uint32_t left_front_sens,
     percent_t race_speed = 30;
     Direction dir = FORWARD;
 
-    percent_t left_speed = delta(left_sens, left_front_sens);
-    percent_t right_speed = delta(right_sens, right_front_sens);
+    uint32_t left_speed = race_speed;
+    uint32_t right_speed = race_speed;
+
+    /* percent_t left_speed = delta(left_sens, left_front_sens); */
+    /* percent_t right_speed = delta(right_sens, right_front_sens); */
 
     /* todo: utilize \back_sens */
 
@@ -62,13 +75,30 @@ void drive::steer(uint32_t left_sens, uint32_t left_front_sens,
     /* left->set(race_speed*left_speed/100, dir); */
     /* right->set(race_speed*right_speed/100, dir); */
 
-    if (left_sens > 1500) {
-        left->set(0, dir);
-        right->set(race_speed, dir);
-    } else {
-        left->set(race_speed);
-        right->set(race_speed);
+    if (left_sens < 1500) {
+        left_speed += 2;
+        right_speed -= 2;
+    } else if (left_sens > 1600) {
+        left_speed -= 2;
+        right_speed += 2;
     }
+
+    if (left_front_sens < 750) {
+        left_speed += (750-left_front_sens)/30;
+        right_speed -= (750-left_front_sens)/30;
+    } else if (left_front_sens > 950) {
+        left_speed -= clamp(left_front_sens, 950, 1500)/68;
+        right_speed += clamp(left_front_sens, 950, 1500)/68;
+    }
+
+    if (left_front_sens < 450) {
+        left_speed = race_speed;
+        right_speed = race_speed/2;
+    }
+
+    left->set(left_speed);
+    right->set(right_speed);
+
 }
 
 /* Local Variables: */
