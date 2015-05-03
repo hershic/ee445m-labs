9f305ad5939a771e96ad0801a8a0d2baa39f9bca
Hershal Bhave <hershal.bhave@gmail.com>
2015-05-03 07:38:37 -0500

    math: ifdef'd

--- board-progs/autonomous-racer/common/math.hpp
+++ board-progs/autonomous-racer/common/math.hpp
@@ -1,3 +1,13 @@
+/* -*- mode: c++; c-basic-offset: 4; -*- */
+#ifndef __math__
+#define __math__
+
+#include <stdint.h>
+
+/*! \addtogroup math
+ * @{
+ */
+
 int32_t clamp(int32_t value, int32_t min, int32_t max) {
     if (value > max) {
         return max;
@@ -15,3 +25,10 @@ inline int32_t floor(int32_t value, int32_t scaling) {
 inline int32_t ceil(int32_t value, int32_t scaling) {
     return (value + scaling)/scaling*scaling;
 }
+
+
+#endif  /* __math__ */
+
+/*! End doxygen group
+ * @}
+ */
