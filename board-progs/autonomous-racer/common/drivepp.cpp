094f1fa7ab87df65bb1e431d33e45f6c6a3b84b5
Hershal Bhave <hershal.bhave@gmail.com>
2015-05-03 02:28:38 -0500

    drive: shitty proportional controller

--- board-progs/autonomous-racer/common/drivepp.cpp
+++ board-progs/autonomous-racer/common/drivepp.cpp
@@ -49,112 +49,38 @@ void drive::steer(uint32_t left_sens, uint32_t left_front_sens,
     /* todo: feed the lf/f, rf/r data here for porportional control of
      * the motors. the side with the larger coefficient slows more */
 
-    percent_t race_speed = 30;
+    percent_t race_speed = 50;
     Direction dir = FORWARD;
 
     uint32_t left_speed = race_speed;
     uint32_t right_speed = race_speed;
 
-    /* percent_t left_speed = delta(left_sens, left_front_sens); */
-    /* percent_t right_speed = delta(right_sens, right_front_sens); */
+    /* int32_t side_error = ((int32_t)left_sens - (int32_t)right_sens)*race_speed/30; */
+    /* int32_t oblique_error = ((int32_t)left_front_sens - (int32_t)right_front_sens)*race_speed/30; */
+    /* int32_t side_error_dir = (side_error < 0)*2-1; */
+    /* int32_t oblique_error_dir = (oblique_error < 0)*2-1; */
 
-    /* todo: utilize \back_sens */
-
-    /* optional: path-centering algorithm */
-
-    /* left->set(race_speed*left_speed/100, dir); */
-    /* right->set(race_speed*right_speed/100, dir); */
-
-
-    int32_t side_error = (int32_t)left_sens - (int32_t)right_sens;
-    int32_t oblique_error = (int32_t)left_front_sens - (int32_t)right_front_sens;
-    int32_t side_error_dir = (side_error < 0)*2-1;
-    int32_t oblique_error_dir = (oblique_error < 0)*2-1;
-
-    /* if (side_error < -100) { */
-    /*     /\* go left *\/ */
-
-    /*     if (side_error > side_error_dir*2000) { */
-    /*         left_speed -= side_error_dir*2; */
-    /*         right_speed += side_error_dir*2; */
-    /*     } else if (side_error > side_error_dir*1500) { */
-    /*         left_speed -= side_error_dir*3; */
-    /*         right_speed += side_error_dir*3; */
-    /*     } else if (side_error > side_error_dir*1000) { */
-    /*         left_speed -= side_error_dir*4; */
-    /*         right_speed += side_error_dir*4; */
-    /*     } else if (side_error > side_error_dir*100) { */
-    /*         left_speed -= side_error_dir*5; */
-    /*         right_speed += side_error_dir*5; */
+    /* if (oblique_error < -100 || oblique_error > 100) { */
+    /*     if ((oblique_error > oblique_error_dir*2000) || (oblique_error < oblique_error_dir*2000)) { */
+    /*         left_speed -= (oblique_error)/race_speed/4; */
+    /*         right_speed += (oblique_error)/race_speed/5; */
+    /*     } else if ((oblique_error > oblique_error_dir*1500) || (oblique_error < oblique_error_dir*1500)) { */
+    /*         left_speed -= (oblique_error)/race_speed/4; */
+    /*         right_speed += (oblique_error)/race_speed/4; */
+    /*     } else if ((oblique_error > oblique_error_dir*1000) || (oblique_error < oblique_error_dir*1000)) { */
+    /*         left_speed -= (oblique_error)/race_speed/4; */
+    /*         right_speed += (oblique_error)/race_speed/4; */
+    /*     } else if ((oblique_error > oblique_error_dir*500) || (oblique_error < oblique_error_dir*500)) { */
+    /*         left_speed -= (oblique_error)/race_speed/4; */
+    /*         right_speed += (oblique_error)/race_speed/4; */
     /*     } */
-
-    /*     /\* left_speed += 2; *\/ */
-    /*     /\* right_speed -= 2; *\/ */
-    /*     /\* left_speed += (-side_error)/race_speed/10; *\/ */
-    /*     /\* right_speed -= (-side_error)/race_speed/10; *\/ */
-    /* } else if (side_error > 100) { */
-    /*     /\* go right *\/ */
-
-    /*     if (side_error > 2000) { */
-    /*         left_speed -= 2; */
-    /*         right_speed += 2; */
-    /*     } else if (side_error > 1500) { */
-    /*         left_speed -= 3; */
-    /*         right_speed += 3; */
-    /*     } else if (side_error > 1000) { */
-    /*         left_speed -= 4; */
-    /*         right_speed += 4; */
-    /*     } else if (side_error > 100) { */
-    /*         left_speed -= 5; */
-    /*         right_speed += 5; */
-    /*     } */
-
-    /*     /\* left_speed -= 2; *\/ */
-    /*     /\* right_speed += 2; *\/ */
-    /*     /\* left_speed -= (side_error)/race_speed/10; *\/ */
-    /*     /\* right_speed += (side_error)/race_speed/10; *\/ */
     /* } */
 
-    if (oblique_error < -100 || oblique_error > 100) {
-        if ((oblique_error > oblique_error_dir*2000) || (oblique_error < oblique_error_dir*2000)) {
-            left_speed -= (oblique_error)/race_speed/2;
-            right_speed += (oblique_error)/race_speed/2;
-        } else if ((oblique_error > oblique_error_dir*1500) || (oblique_error < oblique_error_dir*1500)) {
-            left_speed -= (oblique_error)/race_speed/3;
-            right_speed += (oblique_error)/race_speed/3;
-        } else if ((oblique_error > oblique_error_dir*1000) || (oblique_error < oblique_error_dir*1000)) {
-            left_speed -= (oblique_error)/race_speed/4;
-            right_speed += (oblique_error)/race_speed/4;
-        } else if ((oblique_error > oblique_error_dir*100) || (oblique_error < oblique_error_dir*100)) {
-            left_speed -= (oblique_error)/race_speed/5;
-            right_speed += (oblique_error)/race_speed/5;
-        }
-    }
+    left_speed = 50;
+    right_speed = 50;
 
-    /* almost evade */
-    /* if (left_sens > 3200 || left_front_sens > 3200) { */
-    /*     left_speed -= (oblique_error)/race_speed/6; */
-    /*     right_speed += (oblique_error)/race_speed/6; */
-    /* } */
-
-    /* if (right_sens > 3200 | right_front_sens > 3200) { */
-    /*     left_speed -= (oblique_error)/race_speed/6; */
-    /*     right_speed += (oblique_error)/race_speed/6; */
-    /* } */
-
-    /* evade! */
-    /* if (left_sens > 3350 || left_front_sens > 3350) { */
-    /*     left_speed -= (oblique_error)/race_speed/3; */
-    /*     right_speed += (oblique_error)/race_speed/3; */
-    /* } */
-
-    /* if (right_sens > 3350 | right_front_sens > 3350) { */
-    /*     left_speed -= (oblique_error)/race_speed/3; */
-    /*     right_speed += (oblique_error)/race_speed/3; */
-    /* } */
-
-    left->set(left_speed);
-    right->set(right_speed);
+    left->set(left_speed, dir);
+    right->set(right_speed, dir);
 
 }
 
